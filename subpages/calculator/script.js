83184d728fc30065138e9d527fcfaf1c98865b4a
SamuelCallWind <Samcallwind@gmail.com>
2023-07-20 21:42:50 +0200

    Fixed the addition on 2 numbers. Need to fix the attribution of num1 & 2 after calculating a first time

--- subpages/calculator/script.js
+++ subpages/calculator/script.js
@@ -1,17 +1,3 @@
-let zero = 0;
-let one = 1;
-let two = 2;
-let three = 3;
-let four = 4;
-let five = 5;
-let six = 6;
-let seven = 7;
-let eight = 8;
-let nine = 9;
-let plus = '+';
-let minus = '-';
-let divide = '/';
-let times = '*';
 
 let num1, num2, operator, temporary;
 
@@ -20,14 +6,59 @@ let allButtons = Array.from(document.querySelectorAll('.button'))
 
 for (let i = 0; i < allButtons.length; i++) {
     let currentNumber = allButtons[i];
-    let num = allButtons[i].textContent;
+    let numContent = currentNumber.textContent;
+
     currentNumber.addEventListener('click', function () {
-        if (num === '+' || num === '-' || num === '×' || num === '÷') {
-            operator = currentNumber;
+        if (numContent === '+' || numContent === '-' || numContent === '×' || numContent === '÷') {
+            if (!num1) {
+                num1 = result.value;
+                result.value += numContent;
+                operator = numContent;
+            } else {
+                // if the number 1 is not empty, add the two results because it would mean
+                // that the user has a second addition (e.g 55 + 55 + .....) so we calculate the two first
+            num2 = result.value.slice(num1.length + 1);
+            result.value = calculateTwoNumbers(num1, num2, operator);
             num1 = result.value;
-            result.value = num;
+            num2 = '';
+            operator = '';
+            }
+            // If user click again on an operator, keep only 1
+            if (result.value === '+' || result.value === '-' || result.value === '×' || result.value === '÷'){
+                // result.value = num;
+                console.log('test');
+
+            } 
+
+        } else if (numContent === '=') {
+            result.value = calculateTwoNumbers(num1, num2, numContent);
+        } else if (numContent === 'Reset') {
+            num1 = '';
+            num2 = '';
+            result.value = '';
+            operator = '';
         } else {
-            result.value = result.value + num;
-        }
+            result.value += numContent;
+        } 
     });
+}
+
+function calculateTwoNumbers(num1, num2, operator) {
+    switch (operator) {
+        case '+':
+            return parseInt(num1) + parseInt(num2);
+        case '-':
+            return parseInt(num1) - parseInt(num2);
+        case '×':
+            return parseInt(num1) * parseInt(num2);
+        case '÷':
+            if (num2 !== 0) {
+                return parseInt(num1) / parseInt(num2);
+            } else {
+                return 'Error'
+            }
+        default:
+            return 'Error';
+    }
+
 }

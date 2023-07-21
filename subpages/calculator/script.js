2fe4fe989519321fd6d1b7f20783ab55388ca8b6
SamuelCallWind <Samcallwind@gmail.com>
2023-07-21 08:47:06 +0200

    Fixed the bug after calculating two numbers. The calculator can now do many calculus after one another

--- subpages/calculator/script.js
+++ subpages/calculator/script.js
@@ -14,14 +14,19 @@ for (let i = 0; i < allButtons.length; i++) {
                 num1 = result.value;
                 result.value += numContent;
                 operator = numContent;
-            } else {
+            } else if (operator) {
                 // if the number 1 is not empty, add the two results because it would mean
                 // that the user has a second addition (e.g 55 + 55 + .....) so we calculate the two first
-            num2 = result.value.slice(num1.length + 1);
-            result.value = calculateTwoNumbers(num1, num2, operator);
-            num1 = result.value;
-            num2 = '';
-            operator = '';
+                num2 = result.value.slice(num1.length + 1);
+                result.value = calculateTwoNumbers(num1, num2, operator);
+                num1 = '';
+                num2 = '';
+                result.value = '';
+                operator = '';
+                num1 = result.value;
+            } else {
+                result.value += numContent;
+                operator = numContent;
             }
             // If user click again on an operator, keep only 1
             if (result.value === '+' || result.value === '-' || result.value === '×' || result.value === '÷'){
@@ -31,7 +36,19 @@ for (let i = 0; i < allButtons.length; i++) {
             } 
 
         } else if (numContent === '=') {
-            result.value = calculateTwoNumbers(num1, num2, numContent);
+            num2 = result.value.slice(num1.length + 1);
+            if (calculateTwoNumbers(num1, num2, operator) === 'Error') {
+                num1 = '';
+                num2 = '';
+                result.value = '';
+                operator = '';
+            } else {
+                result.value = calculateTwoNumbers(num1, num2, operator)
+                num1 = result.value;
+                num2 = '';
+                operator = '';
+            }
+            
         } else if (numContent === 'Reset') {
             num1 = '';
             num2 = '';
@@ -55,7 +72,7 @@ function calculateTwoNumbers(num1, num2, operator) {
             if (num2 !== 0) {
                 return parseInt(num1) / parseInt(num2);
             } else {
-                return 'Error'
+                return 'Error'; 
             }
         default:
             return 'Error';

8c9259925961e67faaffe07e58251dd8b5a1e359
SamuelCallWind <Samcallwind@gmail.com>
2023-07-23 13:36:38 +0200

    Lightbulb and lights done on the page. Start / stop button to be done

---
+++ subpages/christmas_lights/script.js
@@ -0,0 +1,10 @@
+let stopLight = document.querySelector('.stopLight');
+let startLight = document.querySelector('.startLight');
+
+stopLight.addEventListener('click', function () {
+    let red = document.querySelector('.red');
+    let blue = document.querySelector('.blue');
+    let green = document.querySelector('.green');
+
+    red.style.animation = 'none';
+});

2cbefeb85d9c5050d443720eb262f40724e95724
SamuelCallWind <Samcallwind@gmail.com>
2023-07-24 09:16:20 +0200

    Added the Javascript to start and stop the lights

--- subpages/christmas_lights/script.js
+++ subpages/christmas_lights/script.js
@@ -1,10 +1,44 @@
-let stopLight = document.querySelector('.stopLight');
-let startLight = document.querySelector('.startLight');
+let stopLight = document.querySelector('#stopLight');
+let startLight = document.querySelector('#startLight');
+
+let red = Array.from(document.querySelectorAll('.red'));
+let blue = Array.from(document.querySelectorAll('.blue'));
+let green = Array.from(document.querySelectorAll('.green'));
 
 stopLight.addEventListener('click', function () {
-    let red = document.querySelector('.red');
-    let blue = document.querySelector('.blue');
-    let green = document.querySelector('.green');
+    for (element of red) {
+        element.style.animation = 'none';
+    }
+    for (element of blue) {
+        element.style.animation = 'none';
+    }
+    for (element of green) {
+        element.style.animation = 'none';
+    }
+});
+startLight.addEventListener('click', function () {
+    red.forEach((element, index) => {
+        if (index % 2 === 0) {
+            element.style.animation = 'glowRed 1s infinite alternate 1s';
+        } else {
+            element.style.animation = 'glowRed 1s infinite alternate 0.5s';
+        }
+    });
 
-    red.style.animation = 'none';
+
+    blue.forEach((element, index) => {
+        if (index % 2 === 0) {
+            element.style.animation = 'glowBlue 1s infinite alternate 0.7s';
+        } else {
+            element.style.animation = 'glowBlue 1s infinite alternate 0.3s';
+        }
+    });
+    green.forEach((element, index) => {
+        if (index % 2 === 0) {
+            element.style.animation = 'glowGreen 1s infinite alternate 1.5s';
+        } else {
+            element.style.animation = 'glowGreen 1s infinite alternate 0.2s';
+        }
+    });
+    
 });
